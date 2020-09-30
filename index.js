6df018bcfa8b2f7f4c3cc6d5f9ff5d710a9509e5
theCelestial25 <37958393+TheCelestial25@users.noreply.github.com>
2020-09-30 20:23:54 +0530

    Add comments and reorganise.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
-const saveInDB = require('./db.js');
-const timeDelay = 30; // time delay in seconds
+const saveInDB = require('./server.js');
+const config = require('./config.js');
 
 // YouTubeAPI requirements
 const {google} = require('googleapis');
@@ -20,6 +20,7 @@ async function runServer() {
 	saveSearch().catch(console.error);
 }
 
+// Just to convert dates into ISO format for google-api and also prevent duplicates
 function updateDate(latestPublished) {
 	var tempDate = new Date(latestPublished);
 	tempDate.setMinutes(tempDate.getMinutes() + 1);
@@ -31,21 +32,24 @@ var latestPublished = new Date();
 latestPublished.setMinutes(latestPublished.getMinutes() - 30);
 latestPublished = latestPublished.toISOString();
 
+
+// Fetch videos from YouTube and store in db
 async function saveSearch() {
 	var done = false;
 	await youtube.search.list({
 		part: 'id,snippet',
-		q: 'dogs',
+		q: config.searchTerm,
 		maxResults: 5,
 		type: 'video',
 		order: 'date',
 		publishedAfter: latestPublished,
 	}).then( result => {
 		if(result.data.items[0]) {
-			console.log("Got the results, now trying to save in db.");
-			console.log(result.data.items);
+			// Uncomment for debugging
+			// console.log("Got the results, now trying to save in db.");
+			// console.log(result.data.items);
 			latestPublished = updateDate(result.data.items[0].snippet.publishedAt);
-			// saveInDB(result.data.items);
+			saveInDB(result.data.items);
 		}
 		else {
 			console.log("No new videos found.");
@@ -55,8 +59,9 @@ async function saveSearch() {
 		console.log(e);
 	});
 
+	// Setting delay
 	if(done) {
-		setTimeout(saveSearch, timeDelay*1000);
+		setTimeout(saveSearch, config.timeDelay*1000);
 	}
 }
 
