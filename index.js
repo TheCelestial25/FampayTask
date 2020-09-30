7bd6f3defeac66781fba3fb2b63a59df9ef7f536
theCelestial25 <37958393+TheCelestial25@users.noreply.github.com>
2020-09-30 15:53:14 +0530

    Complete fetching of videos asynchronously at proper interval.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const saveInDB = require('./db.js');
+const timeDelay = 30; // time delay in seconds
 
 // YouTubeAPI requirements
 const {google} = require('googleapis');
@@ -17,25 +18,46 @@ async function runServer() {
 
 	// call main function after oauth
 	saveSearch().catch(console.error);
-} 
- 
+}
+
+function updateDate(latestPublished) {
+	var tempDate = new Date(latestPublished);
+	tempDate.setMinutes(tempDate.getMinutes() + 1);
+	latestPublished = tempDate.toISOString();
+	return latestPublished;
+}
+
+var latestPublished = new Date();
+latestPublished.setMinutes(latestPublished.getMinutes() - 30);
+latestPublished = latestPublished.toISOString();
+
 async function saveSearch() {
 	var done = false;
 	await youtube.search.list({
 		part: 'id,snippet',
 		q: 'dogs',
-		type: 'video',
 		maxResults: 5,
+		type: 'video',
+		order: 'date',
+		publishedAfter: latestPublished,
 	}).then( result => {
-		console.log("Got the results, now trying to save in db.");
-		saveInDB(result.data.items);
+		if(result.data.items[0]) {
+			console.log("Got the results, now trying to save in db.");
+			console.log(result.data.items);
+			latestPublished = updateDate(result.data.items[0].snippet.publishedAt);
+			saveInDB(result.data.items);
+		}
+		else {
+			console.log("No new videos found.");
+		}
 		done = true;
 	}).catch(e => {
 		console.log(e);
 	});
 
-	if(done)
-		setTimeout(saveSearch, 10000);
+	if(done) {
+		setTimeout(saveSearch, timeDelay*1000);
+	}
 }
 
 runServer().catch(console.error);
